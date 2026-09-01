2468ae1dcfd6a82537c9c9a1947b0d89016d914f
teresahuerta1305-coder <teresa.huerta1305@gmail.com>
2026-09-01 13:00:40 -0600

    Update cargar_datos.py

--- Unidad01/Codigos/cargar_datos.py
+++ Unidad01/Codigos/cargar_datos.py
@@ -1,4 +1,4 @@
 import pandas as pd
-link_csv = "https://raw.githubusercontent.com/teresahuerta1305-coder/EstadisticaInferencial2026A/refs/heads/main/Unidad01/Problemario/datos_problema_01.csv"
+link_csv = ""
 df = pd.read_csv(link_csv)
 df
